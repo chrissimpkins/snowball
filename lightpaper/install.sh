38c67c90ab16715692bd3d873fecceec40577d41
Chris <git.simpkins@gmail.com>
2013-09-17 22:32:30 -0400

    added Lightpaper install script

---
+++ lightpaper/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TARGET_DIR="${HOME}/.lightpaper/Themes/"
+INSTALL_FILE="Snowball.txt"
+SUCCESS_MSG="The Snowball Markdown editor theme was successfully installed.  You can select this theme in LightPaper Preferences > Styles > Theme (MD tab)."
+
+if [ -d "$TARGET_DIR" ]; then
+  cp "$INSTALL_FILE" "$TARGET_DIR"
+  echo "$SUCCESS_MSG"
+else
+  echo "Creating the install directory path..."
+  mkdir -p "$TARGET_DIR"
+  echo "Done. Installing '$INSTALL_FILE'..."
+  cp "$INSTALL_FILE" "$TARGET_DIR"
+  echo "$SUCCESS_MSG"
+fi
+exit 0
